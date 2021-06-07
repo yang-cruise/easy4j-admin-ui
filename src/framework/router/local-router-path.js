23284ecd7731411e130cd94e75babee4a9029c11
kennsand <kennsand@163.com>
2021-06-07 15:55:08 +0800

    fix：跳转页面问题

--- src/framework/router/local-router-path.js
+++ src/framework/router/local-router-path.js
@@ -18,8 +18,8 @@ files.keys().forEach(key => {
 
 bizFiles.keys().forEach(key => {
   const routerPath = `/${key.replace(/(\.\/|\/index\.vue)/g, '')}`
-  const file = bizFiles(key).default || bizFiles(key)
-  const fileName = file.metaTitle
+  const file = bizFiles(key).default || bizFiles(key) || {}
+  const fileName = file.metaTitle || ''
   const currentRouter = {
     path: routerPath,
     component: bizFiles(key).default || bizFiles(key),
